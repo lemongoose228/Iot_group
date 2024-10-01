22e1f2618a76de09d82aff91f1e1ec1718f71df9
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-10-01 10:59:35 +0800

    script for LED was uploaded

--- ledScript.ino
+++ ledScript.ino
@@ -1,1 +1,71 @@
+#define LED_PIN 13
+#define SET_LED_ON 'u'
+#define SET_LED_OFF 'd'
+#define SET_LED_BLINK 'b'
 
+#define STATE_IDLE 0
+#define STATE_LED_ON 1
+#define STATE_LED_OFF 2
+#define STATE_LED_BLINK 3
+
+#define BLINK_INTERVAL 500
+
+int state = 0;
+long int blink_start;
+bool led_status = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void update_state(){
+  if (Serial.available() > 0) {
+    char cmd = Serial.read();
+    if (cmd == SET_LED_ON){
+      state = STATE_LED_ON;
+    }
+
+    else if(cmd == SET_LED_OFF){
+      state = STATE_LED_OFF;
+    }
+
+    else if (cmd == SET_LED_BLINK){
+      state = STATE_LED_BLINK;
+      blink_start = millis();
+    } 
+  }
+}
+
+void process_state(){
+  if (state == STATE_LED_ON){
+    digitalWrite(LED_PIN, HIGH);
+    led_status = HIGH;
+  } 
+
+  else if (state == STATE_LED_OFF){
+    digitalWrite(LED_PIN, LOW);
+    led_status = LOW;
+  }
+
+  else if (state == STATE_LED_BLINK){
+    if (millis() <= blink_start+BLINK_INTERVAL and led_status == LOW){
+      digitalWrite(LED_PIN,HIGH);
+      led_status = HIGH;
+    }
+
+    if (millis() >= blink_start + BLINK_INTERVAL and millis() <= blink_start + 2 * BLINK_INTERVAL and led_status == HIGH){
+      digitalWrite(LED_PIN,LOW);
+      led_status = LOW;
+    }
+
+    if (millis() >= blink_start + 2 * BLINK_INTERVAL){
+      blink_start = millis();
+    }
+  }
+}
+
+void loop() {
+  update_state();
+  process_state();
+}
