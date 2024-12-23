09f7ef451420bcd6c646761373f62d91dd71db79
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-12-24 01:01:14 +0800

    Основной цикл с логикой управления LED

--- ESP_Lamp/pub.py
+++ ESP_Lamp/pub.py
@@ -12,3 +12,37 @@ client.loop_start()
 print("Publishing")
 topic = questionary.text("Print a topic: ").ask()
 print(topic)
+
+counter_start = 0
+counter_end = 0 
+
+while True:
+    current_second = int(time.time() % 60)
+
+    if current_second == 20 + counter_start:
+        print(current_second, "sec")
+        print("Led is up!")
+        client.publish(topic, "1")
+        time.sleep((40 - counter_end) - (20 + counter_start))
+
+    elif current_second == 40 - counter_end:
+        print(current_second, "sec")
+        print("Led is down!")
+        client.publish(topic, "0")
+
+        if counter_end % 2 == 1:
+            counter_start += 1
+
+        counter_end += 1
+        if counter_end == 6:
+            counter_end = 0
+
+        if counter_start == 6:
+            counter_start = 0
+
+        time.sleep(59 - ((40 - counter_end) - (20 + counter_start)))
+
+    else:
+        time.sleep(1)
+client.disconnect()
+client.loop_stop()
