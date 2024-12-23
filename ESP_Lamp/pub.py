45d279ad22c3000fa47d949e97c2eeb693339b78
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-12-24 01:00:36 +0800

    Запрос темы публикации у пользователя

--- ESP_Lamp/pub.py
+++ ESP_Lamp/pub.py
@@ -1,5 +1,6 @@
 import time
 import paho.mqtt.client as paho
+import questionary 
 
 broker = 'broker.emqx.io'
 client = paho.Client("client-isu-003")
@@ -7,3 +8,7 @@ client = paho.Client("client-isu-003")
 print("Connecting to broker ", broker)
 client.connect(broker)
 client.loop_start()
+
+print("Publishing")
+topic = questionary.text("Print a topic: ").ask()
+print(topic)
