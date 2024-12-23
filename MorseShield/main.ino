cb16a9043723fd64a117f16ac8021ed50554247a
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-12-23 15:18:14 +0800

    Добавление алфавита и соответствующих ему кодов Морзе

---
+++ MorseShield/main.ino
@@ -0,0 +1,11 @@
+int mode = 0; 
+int time = 0;
+String textMessage = "";
+String morseMessage = "";
+
+char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "; 
+String morse[] = {
+    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", 
+    "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", 
+    "..-", "...-", ".--", "-..-", "-.--", "--..", " "
+};
