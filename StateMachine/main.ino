61b4671d63fb4dcaae489e427f299e635738771a
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-03-19 04:37:44 +0800

    добавлены функции для движения

--- StateMachine/main.ino
+++ StateMachine/main.ino
@@ -15,3 +15,50 @@ void setup() {
     digitalWrite(i, LOW);
   }
 }
+
+void move(bool dir_left, int speed_left, 
+          bool dir_right, int speed_right) {
+          if (speed_left > 255) {
+            speed_left = 255;
+          }
+
+          if (speed_right > 255) {
+            speed_right = 255;
+          }
+
+          digitalWrite(LEFT_DIR_PIN, dir_left); 
+          analogWrite(LEFT_SPEED_PIN, speed_left); // speed
+
+          digitalWrite(RIGHT_DIR_PIN, dir_right); 
+          analogWrite(RIGHT_SPEED_PIN, speed_right); // speed 
+}
+
+void go_forward(int speed) {
+  move(LEFT_SIDE_FORWARD, speed, RIGHT_SIDE_FORWARD, speed);
+}
+
+void go_backward(int speed) {
+  move(LEFT_SIDE_BACKWARD, speed, RIGHT_SIDE_BACKWARD, speed);
+}
+
+void turn_left(int speed, float ratio) {
+  // ratio between 0 and 1
+  move(LEFT_SIDE_FORWARD, int(speed * ratio), RIGHT_SIDE_FORWARD, speed);
+}
+
+void turn_right(int speed, float ratio) {
+  // ratio between 0 and 1
+  move(LEFT_SIDE_FORWARD, speed, RIGHT_SIDE_FORWARD, int(speed * ratio));
+}
+
+void turn_left_onspot(int speed) {
+  move(LEFT_SIDE_BACKWARD, speed, RIGHT_SIDE_FORWARD, speed);
+}
+
+void turn_right_onspot(int speed) {
+  move(LEFT_SIDE_FORWARD, speed, RIGHT_SIDE_BACKWARD, speed);
+}
+
+void move_only_right() {
+  move(false, 0, true, 200);
+}
