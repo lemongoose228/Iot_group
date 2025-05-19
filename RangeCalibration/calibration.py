a2a3c2cbb9439bd4e5191f77db1c6e83e2de7c69
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-05-20 01:38:03 +0800

    добавлены импорты библиотек

--- RangeCalibration/calibration.py
+++ RangeCalibration/calibration.py
@@ -1,1 +1,6 @@
-
+import serial
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
