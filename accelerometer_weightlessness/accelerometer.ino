c4dc83a3d97a4407fbf993f12e28dd46052860bf
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2025-05-20 02:31:26 +0800

    Настроен ADXL345: включен режим измерения

--- accelerometer_weightlessness/accelerometer.ino
+++ accelerometer_weightlessness/accelerometer.ino
@@ -2,4 +2,9 @@ void setup() {
   Wire.begin(); 
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
+
+  Wire.beginTransmission(SENSOR_ADDR);
+  Wire.write(0x2D);     
+  Wire.write(0x08);    
+  Wire.endTransmission();
 }
