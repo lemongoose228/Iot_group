f66a54f86c3469c88a9f67aa4bb88f0e5166e19f
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2025-05-20 02:32:04 +0800

    Update accelerometer.ino
    Добавлена функция для считывания значений ускорений по осям X, Y, Z

--- accelerometer_weightlessness/accelerometer.ino
+++ accelerometer_weightlessness/accelerometer.ino
@@ -8,3 +8,18 @@ void setup() {
   Wire.write(0x08);    
   Wire.endTransmission();
 }
+
+void readAccel(float &x, float &y, float &z) {
+  Wire.beginTransmission(SENSOR_ADDR);
+  Wire.write(0x32); 
+  Wire.endTransmission(false);
+  Wire.requestFrom(SENSOR_ADDR, 6);
+
+  int16_t rawX = Wire.read() | (Wire.read() << 8);
+  int16_t rawY = Wire.read() | (Wire.read() << 8);
+  int16_t rawZ = Wire.read() | (Wire.read() << 8);
+
+  x = rawX / 256.0;
+  y = rawY / 256.0;
+  z = rawZ / 256.0;
+}
