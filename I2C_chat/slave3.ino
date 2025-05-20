2e71cd7349fc61a20573aee4b4448eefbebafc1b
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-05-20 16:55:17 +0800

    Create slave3.ino

---
+++ I2C_chat/slave3.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+
+String userMessage = "";
+bool sendLength = true;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(10); 
+  Wire.onReceive(receiveHandler);
+  Wire.onRequest(requestHandler);
+  Serial.println("Masha (Slave 10) is ready");
+}
+
+void loop() {
+  if (Serial.available()) {
+    userMessage = readSerialMessage();
+    Serial.print("New message to send: ");
+    Serial.println(userMessage);
+  }
+  delay(100);
+}
+
+void receiveHandler(int howMany) {
+  String incoming = "";
+  for (int i = 0; i < howMany; i++) {
+    if (Wire.available()) {
+      char c = Wire.read();
+      incoming += c;
+    }
+  }
+  Serial.print("Received: ");
+  Serial.println(incoming);
+}
+
+void requestHandler() {
+  if (userMessage.length() > 0) {
+    if (sendLength) {
+      Wire.write((uint8_t)userMessage.length());
+      sendLength = false;
+    } else {
+      Wire.write(userMessage.c_str(), userMessage.length());
+      sendLength = true;
+      userMessage = "";
+    }
+  }
+}
+
+String readSerialMessage() {
+  String message = "";
+  while (Serial.available() == 0) {
+    delay(10);
+  }
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c == '\n') break;
+    message += c;
+    delay(5);
+  }
+  return message;
+}
