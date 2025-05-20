215c6af7d18ba97d74eae1e296b825e74362962b
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-05-20 16:52:13 +0800

    Реализован ввод сообщения пользователем через Serial Monitor

--- I2C_chat/slave1.ino
+++ I2C_chat/slave1.ino
@@ -10,5 +10,25 @@ void setup() {
 }
 
 void loop() {
+  if (Serial.available()) {
+    userMessage = readSerialMessage();
+    Serial.print("New message to send: ");
+    Serial.println(userMessage);
+  }
   delay(100);
 }
+
+String readSerialMessage() {
+  String message = "";
+  while (Serial.available() == 0) {
+    delay(10);
+  }
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c == '\n') break;
+    message += c;
+    delay(5);
+  }
+  return message;
+}
+
