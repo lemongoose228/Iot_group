b5c7b5f4ce2767c71feb8230907b11bc32e30317
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2025-05-28 08:50:47 +0800

    Добавление loop

--- H-bridge/main.ino
+++ H-bridge/main.ino
@@ -8,4 +8,32 @@ const int motorPinB = 5;
   stopMotor();
 }
 
+void loop() {
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
 
+    if (input.length() < 2) {
+      Serial.println("Некорректная команда.");
+      return;
+    }
+
+    char command = input.charAt(0);
+    int speed = input.substring(1).toInt();
+
+    switch (command) {
+      case 'f':
+        setMotorForward(speed);
+        break;
+      case 'b':
+        setMotorBackward(speed);
+        break;
+      case 's':
+        stopMotor();
+        break;
+      default:
+        Serial.println("Неизвестная команда. Используйте f/b/s + скорость");
+        break;
+    }
+  }
+}
